6fc2eda52052c5b4c6d711869c329d881c3cae6f
iamobj <twboss8@126.com>
2021-07-25 15:17:50 +0800

    refactor：具名参数重构

--- sh/update-docker-deploy.sh
+++ sh/update-docker-deploy.sh
@@ -1,30 +1,71 @@
 #!/bin/sh
+### 示例：
+### update-docker-deploy.sh -R ****.com -U iamobj --password 123456 -T "****:123" -A "--restart=always"
+### 参数：
+###   -R, --registry-url              docker 私库地址
+###   -U, --user-name                 私库登录用户名
+###   -P, --password                  私库登录密码
+###   -T, --image-tag                 最新的镜像tag
+###   -N, --container-name            容器名称
+###   -A, --docker-run-args           容器启动参数
 set -e
 
+help() {
+	awk -F'### ' '/^###/ { print $2 }' "$0"
+}
+
+if [[ $# == 0 ]] || [[ "$1" == "-h" ]]; then
+	help
+	exit 1
+fi
+
+ARGS=`getopt -o R:U:P:T:A: -l registry-url:,user-name:,password:,image-tag:,docker-run-args: -n "$0" -- "$@"`
+if [ $? != 0 ]; then
+    echo "终止..."
+    exit 1
+fi
+
 # docker 私库地址
-registry_url=$1
+opt_registry_url=""
 # 私库登录用户名
-user_name=$2
+opt_user_name=""
 # 私库登录密码
-password=$3
+opt_password=""
 # 镜像tag
-image_tag=$4
+opt_image_tag=""
 # 容器名称
-container_name=$5
+opt_container_name=""
 # docker run 参数
-docker_run_params=$6
+opt_docker_run_args=""
+
+# 获取参数
+while [ -n "$1" ]
+do
+  case "$1" in
+    -R|--registry-url) opt_registry_url=$2; shift 2;;
+    -U|--user-name) opt_user_name=$2; shift 2;;
+    -P|--password) opt_password=$2; shift 2;;
+    -T|--image-tag) opt_image_tag=$2; shift 2;;
+    -N|--container-name) opt_container_name=$2; shift 2;;
+    -A|--docker-run-args) opt_docker_run_args=$2; shift 2;;
+    --) break ;;
+    *) echo $1,$2,$show_usage; break ;;
+  esac
+done
 
 # 停止容器并删除容器
-docker stop $container_name || true && docker rm $container_name || true
+docker stop $opt_container_name || true && docker rm $opt_container_name || true
 
 # 登录镜像私库
-docker login $registry_url -u $user_name -p $password
+docker login $opt_registry_url -u $opt_user_name -p $opt_password
 
 # 拉取镜像
-docker pull $image_tag
+docker pull $opt_image_tag
 
 # 退出私库
-docker logout $registry_url
+docker logout $opt_registry_url
 
 # 启动
-docker run --name $container_name $docker_run_params -d $image_tag
+docker run --name $opt_container_name $opt_docker_run_args -d $image_tag
+
+printf -- '\n'
